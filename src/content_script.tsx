157859e63fca03830c3e1693d32e6d6d434e1a2c
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-05-03 16:58:01 +0900

    未読取得処理を追加

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,9 +1,17 @@
-chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-  if (msg.color) {
-    console.log("Receive color = " + msg.color);
-    document.body.style.backgroundColor = msg.color;
-    sendResponse("Change color to " + msg.color);
-  } else {
-    sendResponse("Color message is none.");
+const checkNewNotification = () => {
+  for (let i = 0; i < document.getElementById("RoomList")!.children!.length; i++) {
+    let tabElements = document.getElementById("RoomList")!.children![i];
+    for (let n = 0; n < tabElements.children!.length; n++) {
+      let roomTabElement = tabElements.children![n];
+      const rid = roomTabElement!.getAttribute("data-rid");
+      const roomName = roomTabElement!.getAttribute("aria-label");
+      const unread = roomTabElement.getElementsByClassName("_unreadBadge").length !== 0;
+      console.log(`${rid}:${roomName}:${unread ? "未読あり" : "未読無し"}`);
+    }
   }
-});
+};
+const check = () => {
+  checkNewNotification();
+};
+
+const loop = setInterval(check, 5000);
