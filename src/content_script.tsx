177c10a1856961dc16de6ac8c072e050b5c22b5b
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-05-06 17:22:01 +0900

    未読へ状態変化しているもののみを取得へ

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,14 +1,31 @@
-const checkNewNotification = () => {
-  for (let i = 0; i < document.getElementById("RoomList")!.children!.length; i++) {
-    let tabElements = document.getElementById("RoomList")!.children![i];
-    for (let n = 0; n < tabElements.children!.length; n++) {
-      let roomTabElement = tabElements.children![n];
-      const rid = roomTabElement!.getAttribute("data-rid");
-      const roomName = roomTabElement!.getAttribute("aria-label");
-      const unread = roomTabElement.getElementsByClassName("_unreadBadge").length !== 0;
-      console.log(`${rid}:${roomName}:${unread ? "未読あり" : "未読無し"}`);
+import { BackgroundMessage } from "./interface";
+import { ChatworkRoomTable } from "./interface/dbTable";
+
+const checkNewNotification = async () => {
+  const sendMessage: BackgroundMessage = { requestKind: "getChatworkRoom" };
+  chrome.runtime.sendMessage<BackgroundMessage, ChatworkRoomTable[] | undefined>(
+    sendMessage,
+    (response) => {
+      if (response === undefined) return;
+      //状態が変わっているRoomをdiffListに格納する
+      const diffList: ChatworkRoomTable[] = [];
+      for (let i = 0; i < document.getElementById("RoomList")!.children!.length; i++) {
+        let tabElements = document.getElementById("RoomList")!.children![i];
+        for (let n = 0; n < tabElements.children!.length; n++) {
+          let roomTabElement = tabElements.children![n];
+          const rid = roomTabElement!.getAttribute("data-rid");
+          const roomName = roomTabElement!.getAttribute("aria-label");
+          const unread = roomTabElement.getElementsByClassName("_unreadBadge").length !== 0;
+          if (response.filter((room) => room.rid === rid).length === 0) continue;
+          const targetRoom = response.filter((room) => room.rid === rid)[0];
+          if (unread && targetRoom.status !== "unread") {
+            diffList.push(targetRoom);
+          }
+        }
+      }
+      //diffListをDBに適用する
     }
-  }
+  );
 };
 const check = () => {
   //未読メッセージの確認
