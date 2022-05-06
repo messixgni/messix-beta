cb90edbc3a3381ab3f5c33b7a874f234b4230901
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-05-06 13:33:16 +0900

    DBとの通信処理を追加

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,36 @@
-function polling() {
-  // console.log("polling");
-  setTimeout(polling, 1000 * 30);
-}
+import { db, MessixDB } from "./db";
+import { BackgroundMessage, BgMsgChatworkRoom } from "./interface";
 
-polling();
+const postChatworkRoom = async (bgMsgChatworkRoom: BgMsgChatworkRoom) => {
+  try {
+    const result = await db.chatworkRoom.add(bgMsgChatworkRoom.data!);
+  } catch (err) {
+    return false;
+  }
+  return true;
+};
+const getChatworkRoom = async (bgMsgChatworkRoom: BgMsgChatworkRoom) => {
+  try {
+    const rooms = await db.chatworkRoom.toArray();
+    return rooms;
+  } catch (err) {
+    return undefined;
+  }
+};
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  const backgroundMessage: BackgroundMessage = message;
+  switch (backgroundMessage.requestKind) {
+    case "postChatworkRoom":
+      const res = await postChatworkRoom(message);
+      sendResponse(res);
+      break;
+    case "getChatworkRoom":
+    case "putChatworkRoom":
+    case "deleteChatworkRoom":
+      break;
+
+    case "postChatworkMessage":
+      break;
+  }
+});
