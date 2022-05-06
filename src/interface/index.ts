d7be48cbf3ee5f192b71f98fcce918933d014215
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-05-06 18:21:05 +0900

    deleteMethodを削除、roomのリストを想定した型を追加

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,15 +1,13 @@
 import { ChatworkRoomTable } from "./dbTable";
 
 export type BackgroundMessage = {
-  requestKind:
-    | "postChatworkRoom"
-    | "getChatworkRoom"
-    | "putChatworkRoom"
-    | "deleteChatworkRoom"
-    | "postChatworkMessage";
+  requestKind: "postChatworkRoom" | "getChatworkRoom" | "putChatworkRoom" | "postChatworkMessage";
 };
 export type BgMsgChatworkRoom = BackgroundMessage & {
   data?: ChatworkRoomTable;
+};
+export type BgMsgChatworkRooms = BackgroundMessage & {
+  data?: ChatworkRoomTable[];
 };
 
 export type ChatworkRoom = {
