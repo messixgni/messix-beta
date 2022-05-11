7bae930489339f0d7ad9d36980cc5f61eecd67cb
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-05-11 16:52:30 +0900

    チャット中のメッセージ一覧を取得する処理を追加

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,5 +1,5 @@
-import { BackgroundMessage, BgMsgChatworkRooms } from "./interface";
-import { ChatworkRoomTable } from "./interface/dbTable";
+import { BackgroundMessage, BgMsgChatworkRooms, MessageUser } from "./interface";
+import { ChatworkRoomTable, ChatworkMessageTable } from "./interface/dbTable";
 
 const checkNewNotification = async () => {
   const sendMessage: BackgroundMessage = { requestKind: "getChatworkRoom" };
@@ -35,6 +35,66 @@ const checkNewNotification = async () => {
     }
   );
 };
+type GetMessixUserMessageUser = () => MessageUser | undefined;
+const getMessixUserMessageUser: GetMessixUserMessageUser = () => {
+  const roomMemberAreaElement = document.getElementById("roomMemberArea");
+  if (!roomMemberAreaElement) return undefined;
+  const userImgs = roomMemberAreaElement.getElementsByTagName("img");
+  if (userImgs.length === 0) return undefined;
+  const name = userImgs[0].getAttribute("alt");
+  const iconUrl = userImgs[0].getAttribute("src");
+  if (name === null || iconUrl === null) return undefined;
+  return {
+    name: name,
+    iconUrl: iconUrl,
+  };
+};
+const getMessageUser = (doc: Element) => {
+  const speakers = doc.getElementsByClassName("_speaker");
+  if (speakers.length === 0) return undefined;
+  const imgElements = speakers[0].getElementsByTagName("img");
+  if (imgElements.length === 0) return undefined;
+  const name = imgElements[0].getAttribute("alt");
+  const iconUrl = imgElements[0].getAttribute("src");
+  if (name === null || iconUrl === null) return undefined;
+  const messageUser: MessageUser = {
+    name: name,
+    iconUrl: iconUrl,
+  };
+  return messageUser;
+};
+type GetMessages = () => ChatworkMessageTable[];
+const getMessages: GetMessages = () => {
+  let rtnArray: ChatworkMessageTable[] = [];
+  let messixUser = getMessixUserMessageUser();
+  if (!messixUser) return rtnArray;
+  const timeLineEmenents = document.getElementById("_timeLine");
+  const messageElements = timeLineEmenents?.getElementsByClassName("_message");
+  if (!messageElements) return rtnArray;
+  let currentUser: MessageUser | undefined;
+  for (let i = 0; i < messageElements.length; i++) {
+    const tmpUser = getMessageUser(messageElements[i]);
+    if (tmpUser !== undefined) currentUser = tmpUser;
+    const spanElements = messageElements[i].getElementsByTagName("span");
+    const rid = messageElements[i].getAttribute("data-rid");
+    if (spanElements.length === 0 || rid === null) continue;
+    const content = spanElements[0].textContent;
+    const unixTime = spanElements[0]
+      .getElementsByClassName("_timeStamp")[0]
+      .getAttribute("data-tm");
+    if (content === null || unixTime === null) continue;
+    const message: ChatworkMessageTable = {
+      roomId: parseInt(rid),
+      name: currentUser?.name!,
+      content: content,
+      createAt: new Date(parseInt(unixTime) * 1000),
+    };
+    rtnArray.push(message);
+  }
+
+  return rtnArray;
+};
+const checkReply = () => {};
 const check = () => {
   //未読メッセージの確認
   checkNewNotification();
