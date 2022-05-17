3d2a48c533d94cb296e73371dcacf41d1d989253
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-05-17 14:05:37 +0900

    設定ページへの遷移処理を作成

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,12 +5,14 @@ import { ChatworkRoom } from "./interface";
 import { db } from "./db";
 import { useLiveQuery } from "dexie-react-hooks";
 import UnreplyListItem from "./components/UnreplyListItem";
+import SettingPage from "./components/SettignPage";
 
 const Popup = () => {
   const [unmanagedRoom, setUnmanagedRoom] = useState<ChatworkRoom>();
   const unreads = useLiveQuery(() => db.chatworkRoom.where("status").equals("unread").toArray());
   const unreplys = useLiveQuery(() => db.chatworkRoom.where("status").equals("unreply").toArray());
   const [isUnreadView, setIsUnreadView] = useState<boolean>(true);
+  const [isSettingView, setIsSettingView] = useState<boolean>(false);
   useEffect(() => {
     const getBrowserActiveTabInfo = async () => {
       let queryOptions = { active: true, currentWindow: true };
@@ -38,62 +40,86 @@ const Popup = () => {
     <div style={{ width: "600px" }}>
       <Container fluid>
         <Row style={{ height: "50px", backgroundColor: "lightgray" }}>
-          {unmanagedRoom ? (
+          {isSettingView ? (
+            <></>
+          ) : (
             <>
-              <Col xs={10} className="d-flex align-items-center">
-                <p>『{unmanagedRoom.name}』のチャットを通知管理しますか？</p>
+              {unmanagedRoom ? (
+                <>
+                  <Col xs={10} className="d-flex align-items-center">
+                    <p>『{unmanagedRoom.name}』のチャットを通知管理しますか？</p>
+                  </Col>
+                  <Col xs={2} className="d-flex align-items-center">
+                    <Button onClick={onClickAddManageBtn}>はい</Button>
+                  </Col>
+                </>
+              ) : (
+                <></>
+              )}
+            </>
+          )}
+        </Row>
+        <Row style={{ height: "400px" }}>
+          {unreads && unreplys ? (
+            <>
+              <Col xs={2} style={{ borderRight: "5px solid lightgray" }}>
+                <Row>
+                  <div
+                    className={
+                      "d-flex align-items-center popup-tab" +
+                      (isUnreadView ? " popup-tab-selected" : "")
+                    }
+                    onClick={() => setIsUnreadView(true)}
+                  >
+                    <p>未読{unreads.length === 0 ? "" : ` [${unreads.length}]`}</p>
+                  </div>
+                </Row>
+                <Row>
+                  <div
+                    className={
+                      "d-flex align-items-center popup-tab" +
+                      (isUnreadView ? "" : " popup-tab-selected")
+                    }
+                    onClick={() => setIsUnreadView(false)}
+                  >
+                    <p>未返信</p>
+                  </div>
+                </Row>
+                <Row style={{ height: "300px" }} className="d-flex align-items-end">
+                  <Button
+                    className="mb-2"
+                    onClick={() => {
+                      setIsSettingView(!isSettingView);
+                    }}
+                  >
+                    {isSettingView ? "戻る" : "設定"}
+                  </Button>
+                </Row>
               </Col>
-              <Col xs={2} className="d-flex align-items-center">
-                <Button onClick={onClickAddManageBtn}>はい</Button>
+              <Col xs={10}>
+                {isSettingView ? (
+                  <SettingPage />
+                ) : (
+                  <>
+                    {isUnreadView ? (
+                      <a href="https://www.chatwork.com" target="_blank">
+                        {`未読メッセージが${unreads.length}件あります`}
+                      </a>
+                    ) : (
+                      <>
+                        {unreplys.map((unreply) => (
+                          <UnreplyListItem chatworkRoom={unreply} />
+                        ))}
+                      </>
+                    )}
+                  </>
+                )}
               </Col>
             </>
           ) : (
             <></>
           )}
         </Row>
-        {unreads && unreplys ? (
-          <Row style={{ height: "400px" }}>
-            <Col xs={2} style={{ borderRight: "5px solid lightgray" }}>
-              <Row>
-                <div
-                  className={
-                    "d-flex align-items-center popup-tab" +
-                    (isUnreadView ? " popup-tab-selected" : "")
-                  }
-                  onClick={() => setIsUnreadView(true)}
-                >
-                  <p>未読{unreads.length === 0 ? "" : ` [${unreads.length}]`}</p>
-                </div>
-              </Row>
-              <Row>
-                <div
-                  className={
-                    "d-flex align-items-center popup-tab" +
-                    (isUnreadView ? "" : " popup-tab-selected")
-                  }
-                  onClick={() => setIsUnreadView(false)}
-                >
-                  <p>未返信</p>
-                </div>
-              </Row>
-            </Col>
-            <Col xs={10}>
-              {isUnreadView ? (
-                <a href="https://www.chatwork.com" target="_blank">
-                  {`未読メッセージが${unreads.length}件あります`}
-                </a>
-              ) : (
-                <>
-                  {unreplys.map((unreply) => (
-                    <UnreplyListItem chatworkRoom={unreply} />
-                  ))}
-                </>
-              )}
-            </Col>
-          </Row>
-        ) : (
-          <></>
-        )}
       </Container>
     </div>
   );
