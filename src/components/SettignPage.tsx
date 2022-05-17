3aa24a16424f622781be2f2245a6542487dabd7d
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-05-17 14:05:21 +0900

    設定ページを作成

--- src/components/SettignPage.tsx
+++ src/components/SettignPage.tsx
@@ -1,11 +1,31 @@
+import { useLiveQuery } from "dexie-react-hooks";
 import React from "react";
 import { Form } from "react-bootstrap";
+import { db } from "../db";
+import { ChatworkRoomTable } from "../interface/dbTable";
+
+type ChatworkRoomActiveSwitchProps = {
+  room: ChatworkRoomTable;
+};
+const ChatworkRoomActiveSwitch = ({ room }: ChatworkRoomActiveSwitchProps) => {
+  const onChange = () => {
+    room.isActive = !room.isActive;
+    db.chatworkRoom.put(room);
+  };
+  return (
+    <Form.Switch type="switch" label={room.name} checked={room.isActive} onChange={onChange} />
+  );
+};
 
 const SettingPage = () => {
+  const chatworkRooms = useLiveQuery(() => db.chatworkRoom.toArray());
   return (
     <>
+      <h2>設定{"(通知管理)"}</h2>
       <Form>
-        <Form.Switch type="switch" label="〇〇ワークスペース" checked={true} />
+        {chatworkRooms?.map((cr) => (
+          <ChatworkRoomActiveSwitch room={cr} />
+        ))}
       </Form>
     </>
   );
