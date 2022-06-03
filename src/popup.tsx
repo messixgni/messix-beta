f940c1cfd5c71e9ea2446ff463b1f71545fbeac1
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-06-03 15:59:19 +0900

    google analysticを無効化

--- src/popup.tsx
+++ src/popup.tsx
@@ -143,12 +143,14 @@ const Popup = () => {
   );
 };
 
+/*
 window.onload = function () {
   const scriptTag = document.createElement("script");
   scriptTag.src = chrome.runtime.getURL("./analytics-override.js");
   scriptTag.type = "text/javascript";
   document.head.appendChild(scriptTag);
 };
+*/
 
 ReactDOM.render(
   <React.StrictMode>
