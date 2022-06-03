ec074b3ec5d5dc57057dd03604f63260c32120fe
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-06-04 00:12:32 +0900

    実際に動くコードに変更・未読の部屋一覧が見れるように変更

--- src/popup.tsx
+++ src/popup.tsx
@@ -14,7 +14,7 @@ const Popup = () => {
   const [unmanagedRoom, setUnmanagedRoom] = useState<ChatworkRoom>();
   const unreads = useLiveQuery(() => db.chatworkRoom.where("status").equals("unread").toArray());
   const unreplys = useLiveQuery(() => db.chatworkRoom.where("status").equals("unreply").toArray());
-  const [isUnreadView, setIsUnreadView] = useState<boolean>(true);
+  const [isUnreadView, setIsUnreadView] = useState<boolean>(false);
   const [isSettingView, setIsSettingView] = useState<boolean>(false);
   const [lastChangedRoom, setLastChangedRoom] = useState<ChatworkRoomTable>();
   useEffect(() => {
@@ -42,67 +42,115 @@ const Popup = () => {
     });
     setUnmanagedRoom(undefined);
   };
+  const getCountBadge = (datas: ChatworkRoom[] | undefined) => {
+    if (datas) {
+      if (datas.length === 0) return <></>;
+      return <span className="badge rounded-pill bg-danger">{datas.length}</span>;
+    }
+    return <></>;
+  };
   return (
     <div className="d-flex flex-row" style={{ width: "600px" }}>
       <div className="sidebar d-flex flex-column flex-shrink-0 p-3 bg-light">
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+        <a
+          href="/"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+        >
           <img src="messix-logo.png" width="103" height="41" />
           <span className="fs-4"></span>
         </a>
         <hr />
         <ul className="nav nav-pills flex-column mb-auto">
           <li className="nav-item">
-            <a href="#" className={"nav-link active"} aria-current="page" onClick={() => setIsUnreadView(true)}>
-              <img src="unread.png" width="16" height="16" /> 未読 <span className="badge rounded-pill bg-danger">{
-                //unreads && unreads.length === 0 ? "" : ` [${unreads && unreads.length}]`
-                10
-              }</span>
+            <a
+              href="#"
+              className={isUnreadView ? "nav-link active" : "nav-link link-dark"}
+              aria-current="page"
+              onClick={() => setIsUnreadView(true)}
+            >
+              <img src="unread.png" width="16" height="16" /> 未読 {getCountBadge(unreads)}
             </a>
           </li>
           <li className="nav-item">
-            <a href="#" className={"nav-link link-dark"} aria-current="page" onClick={() => setIsUnreadView(false)}>
-              <img src="unreply.png" width="16" height="16" /> 未返信{unreads && unreads.length === 0 ? "" : ` [${unreads && unreads.length}]`}
+            <a
+              href="#"
+              className={!isUnreadView ? "nav-link active" : "nav-link link-dark"}
+              aria-current="page"
+              onClick={() => setIsUnreadView(false)}
+            >
+              <img src="unreply.png" width="16" height="16" /> 未返信 {getCountBadge(unreplys)}
             </a>
           </li>
         </ul>
         <div className="sidecar__btm align-items-end">
           <hr />
-          <a href="#" className="d-flex link-dark text-decoration-none" onClick={() => { setIsSettingView(!isSettingView); }}>
-            <img src="settings.png" alt="" width="16" height="16" className="me-2" /> 設定
+          <a
+            href="#"
+            className="d-flex link-dark text-decoration-none"
+            onClick={() => {
+              setIsSettingView(!isSettingView);
+            }}
+          >
+            <img src="settings.png" alt="" width="16" height="16" className="me-2" />{" "}
+            {isSettingView ? "戻る" : "設定"}
           </a>
         </div>
       </div>
       <div className="MainContents">
-        {//unmanagedRoom ? ( 
-          true ? (
-            <div className="managementNotification alert alert-info alert-dismissible fade show" role="alert">
-              <p>『<strong>{/*unmanagedRoom.name */ "○○のお部屋"}</strong>』のチャットを通知管理しますか？</p>
-              <Button className="btn-sm btn-primary" onClick={onClickAddManageBtn}>はい</Button>
-              <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-
-          ) : (
-            <></>
-          )}
-        <Col xs={10}>
+        {unmanagedRoom ? (
+          <div
+            className="managementNotification alert alert-info alert-dismissible fade show"
+            role="alert"
+          >
+            <p>
+              『<strong>{unmanagedRoom.name}</strong>』のチャットを通知管理しますか？
+            </p>
+            <Button className="btn-sm btn-primary" onClick={onClickAddManageBtn}>
+              はい
+            </Button>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+              onClick={() => {
+                setUnmanagedRoom(undefined);
+              }}
+            ></button>
+          </div>
+        ) : (
+          <></>
+        )}
+        <div>
           {isSettingView ? (
             <SettingPage />
           ) : (
             <>
               {isUnreadView ? (
-                <a href="https://www.chatwork.com" target="_blank">
-                  {`未読メッセージが${unreads && unreads.length}件あります`}
-                </a>
+                <>
+                  {unreads ? (
+                    <>
+                      {unreads.map((unread) => (
+                        <a href={`https://chatwork.com#!rid${unread.rid}`} target="_blank">
+                          {unread.name}
+                        </a>
+                      ))}
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </>
               ) : (
                 <>
-                  {unreplys && unreplys.map((unreply) => (
-                    <UnreplyListItem
-                      chatworkRoom={unreply}
-                      onChangeToNormal={(chatworkRoom) => {
-                        setLastChangedRoom(chatworkRoom);
-                      }}
-                    />
-                  ))}
+                  {unreplys &&
+                    unreplys.map((unreply) => (
+                      <UnreplyListItem
+                        chatworkRoom={unreply}
+                        onChangeToNormal={(chatworkRoom) => {
+                          setLastChangedRoom(chatworkRoom);
+                        }}
+                      />
+                    ))}
                 </>
               )}
             </>
@@ -113,7 +161,7 @@ const Popup = () => {
               setLastChangedRoom(undefined);
             }}
           />
-        </Col>
+        </div>
       </div>
     </div>
   );
