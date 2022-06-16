ab2761f932ff70be43d83718c3e68230f26a06fa
KentaroJay <kentaro.jay.takahashi@gmail.com>
2022-06-16 19:57:19 +0900

    add class "img-fluid" on img tag.

--- src/popup.tsx
+++ src/popup.tsx
@@ -196,8 +196,11 @@ const Popup = () => {
           ) : (
             <>
               {isUnreadView ? (
-                  <>
-                    <div className="d-flex align-items-center h2 fw-bold m-2"><img src="icon_chatwork.png" style={{ height: "1em" }} />Chatwork</div>
+                <>
+                  <div className="d-flex align-items-center h2 fw-bold m-2">
+                    <img src="icon_chatwork.png" className="img-fluid" style={{height: "1em"}} />
+                    Chatwork
+                  </div>
                   {unreads ? (
                     <>
                       {unreads.map((unread) => (
@@ -206,7 +209,7 @@ const Popup = () => {
                             className="d-inline-block text-truncate text-decoration-none text-reset stretched-link m-2"
                             href={`https://chatwork.com#!rid${unread.rid}`}
                             target="_blank"
-                            style={{maxWidth:"190px"}}
+                            style={{ maxWidth: "190px" }}
                           >
                             {unread.name}
                           </a>
