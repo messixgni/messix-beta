ddcd0d5673609ed1165c33af76c5f61a84f1dbbc
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-06-20 17:08:01 +0900

    更新処理を追加

--- src/components/UnreplyList.tsx
+++ src/components/UnreplyList.tsx
@@ -1,14 +1,37 @@
 import React from "react";
+import { db } from "../db";
 import { useUnreplys } from "../hook/useUnreplys";
+import { ChatworkMessageStatusTable, ChatworkMessageTable } from "../interface/dbTable";
 import UnreplyListItem from "./UnreplyListItem";
 
 const UnreplyList = () => {
   const { isLoading, messages, changeStatus } = useUnreplys();
   if (isLoading) return <p>Loading...</p>;
+  const onChangeMessage = (chatworkMessage: ChatworkMessageTable & ChatworkMessageStatusTable) => {
+    if (chatworkMessage.isUnreply === 0) {
+    }
+    const dbChange = async (messageStatus: ChatworkMessageStatusTable) => {
+      const res = await db.chatworkMessageStatus
+        .where("messageId")
+        .equals(messageStatus.messageId)
+        .first();
+      await db.chatworkMessageStatus.put({
+        id: res?.id,
+        messageId: messageStatus.messageId,
+        isUnreply: messageStatus.isUnreply,
+        isMarked: messageStatus.isMarked,
+      });
+    };
+    dbChange({
+      messageId: chatworkMessage.messageId,
+      isUnreply: chatworkMessage.isUnreply,
+      isMarked: chatworkMessage.isMarked,
+    });
+  };
   return (
     <>
       {messages?.map((message) => (
-        <UnreplyListItem chatworkMessage={message} onChangeToNormal={() => {}} />
+        <UnreplyListItem chatworkMessage={message} onChange={onChangeMessage} />
       ))}
     </>
   );
