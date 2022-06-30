3a518d655b41b385820c7c66f6f205a6d6d9c5f0
Chroma7p <nahco3.hcl@gmail.com>
2022-06-30 23:11:05 +0900

    スタンプで解決の項目を作成し、スイッチを設置、変更時の処理は未完成

--- src/components/SettignPage.tsx
+++ src/components/SettignPage.tsx
@@ -1,8 +1,10 @@
 import { useLiveQuery } from "dexie-react-hooks";
-import React from "react";
+import React, { ReactText } from "react";
 import { Form } from "react-bootstrap";
 import { db } from "../db";
 import { ChatworkRoomTable } from "../interface/dbTable";
+import { Stamp } from "../interface/setting";
+import { allStamps, getCurrentSetting } from "../util";
 
 type ChatworkRoomActiveSwitchProps = {
   room: ChatworkRoomTable;
@@ -20,12 +22,37 @@ const ChatworkRoomActiveSwitch = ({ room }: ChatworkRoomActiveSwitchProps) => {
 
 const SettingPage = () => {
   const chatworkRooms = useLiveQuery(() => db.chatworkRoom.toArray());
+  const onChangeStampSwitch = (e: React.ChangeEvent<HTMLInputElement>, stamp: { name: Stamp; title: string }) => {
+
+  };
   return (
     <>
       <h2>設定{"(通知管理)"}</h2>
       <Form>
         {chatworkRooms?.map((cr) => (
           <ChatworkRoomActiveSwitch room={cr} />
+        ))}
+      </Form>
+
+      <h2>スタンプで解決</h2>
+      <p>
+        メンションがついているメッセージにスタンプを付けることで、返信として扱うオプションです。
+      </p>
+      <h3>対象スタンプ</h3>
+      <Form>
+        {allStamps.map((stamp) => (
+          <Form.Switch
+            type="switch"
+            label={stamp.title}
+            checked={
+              getCurrentSetting()?.autoChangeMessageStatusStamps?.find(
+                (st) => st === stamp.name
+              ) !== undefined
+            }
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              onChangeStampSwitch(e, stamp);
+            }}
+          />
         ))}
       </Form>
     </>
