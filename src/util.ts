8c2276c90506ae7e97c39105d199759cc8893d86
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2022-06-30 22:22:25 +0900

    アプリ起動時に設定ファイルを検証して足りない項目を追加する処理を追加

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import { db } from "./db";
+import { Setting } from "./interface/setting";
 export const changeBadgeText = async () => {
   //const unreadCount = await db.chatworkRoom.filter((cr) => cr.status === "unread").count();
   //chrome.action.setBadgeText({ text: unreadCount === 0 ? "" : unreadCount.toString() });
@@ -11,3 +12,29 @@ export const getTimePastStatus = (time: Date): string => {
   if (hours > 3) return "yellow";
   return "normal";
 };
+
+const checkSettingFormat = () => {
+  const settingJson = localStorage.getItem("messix-setting");
+  if (!settingJson) {
+    //設定ファイルの生成
+    const newSetting: Setting = {
+      howToRestartNotifDone: false,
+      autoChangeMessageStatusStamps: ["roger", "bow", "cracker", "dance", "clap", "yes"],
+    };
+    localStorage.setItem("messix-setting", JSON.stringify(newSetting));
+  } else {
+    const currentSetting: Setting = JSON.parse(settingJson);
+    if (!currentSetting.autoChangeMessageStatusStamps) {
+      currentSetting.autoChangeMessageStatusStamps = [
+        "roger",
+        "bow",
+        "cracker",
+        "dance",
+        "clap",
+        "yes",
+      ];
+    }
+    localStorage.setItem("messix-setting", JSON.stringify(currentSetting));
+  }
+};
+checkSettingFormat();
