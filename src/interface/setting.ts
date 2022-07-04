60a830f16aa3c5fb9441f6ff90a9e7b54e1cf568
KentaroJay <kentaro.jay.takahashi@gmail.com>
2022-07-04 15:27:46 +0900

    content scriptでスタンプの情報とってくる処理

--- src/interface/setting.ts
+++ src/interface/setting.ts
@@ -1,6 +1,10 @@
-type Stamp = "roger" | "bow" | "cracker" | "dance" | "clap" | "yes";
+export type Stamp = "roger" | "bow" | "cracker" | "dance" | "clap" | "yes";
 
 export type Setting = {
   howToRestartNotifDone: boolean;
   autoChangeMessageStatusStamps?: Stamp[];
+};
+
+export type Stamps = {
+  stamps: Stamp[];
 };
