2e60fe387d134c738d8afd7c69155878fbab639d
KentaroJay <kentaro.jay.takahashi@gmail.com>
2022-07-08 22:49:37 +0900

    以下を実装 ・モーダル ・クリップボードへのコピー
    実装していないこと
    ・デザイン

--- src/components/SettignPage.tsx
+++ src/components/SettignPage.tsx
@@ -1,6 +1,7 @@
 import { useLiveQuery } from "dexie-react-hooks";
+import { useState } from "react";
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button, Modal } from "react-bootstrap";
 import { db } from "../db";
 import { ChatworkRoomTable } from "../interface/dbTable";
 
@@ -19,7 +20,14 @@ const ChatworkRoomActiveSwitch = ({ room }: ChatworkRoomActiveSwitchProps) => {
 };
 
 const SettingPage = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const chatworkRooms = useLiveQuery(() => db.chatworkRoom.toArray());
+  const copyToClipboard = (target: string) => {
+    const copyTarget = document.getElementById(target)!;
+    navigator.clipboard.writeText(copyTarget.innerText);
+  }
   return (
     <>
       <h2>設定{"(通知管理)"}</h2>
@@ -28,6 +36,27 @@ const SettingPage = () => {
           <ChatworkRoomActiveSwitch room={cr} />
         ))}
       </Form>
+      <hr />
+      <Button variant="danger" onClick={handleShow}>
+        バグを報告する
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            バグ報告フォームへ遷移
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div id="bug-report-text-1">Woohoo, you're reading this text in a modal!</div>
+          <Button onClick={() => { copyToClipboard("bug-report-text-1"); }}>コピー</Button>
+        </Modal.Body>
+        <Modal.Footer>
+          <a href="./">バグ報告フォームへ遷移する</a>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
